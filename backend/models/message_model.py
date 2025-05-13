7b55d5f7f39a2b5f2951e0522deacb37d017cae9
Stefan Dimitrov <svdimitrv@gmail.com>
2025-05-13 22:22:02 +0200

    Update ruff.toml

--- backend/models/message_model.py
+++ backend/models/message_model.py
@@ -4,8 +4,8 @@ from sqlalchemy.sql import func
 from ..database import Base
 
 
-class Message(Base):
-    __tablename__ = "messages"
+class MessageModel(Base):
+    __tablename__ = 'messages'
 
     id = Column(Integer, primary_key=True, index=True)
     content = Column(String)
