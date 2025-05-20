072a52e66becf90379736b2c0a64112d31abf16a
Fatairy <elfatairyomar@gmail.com>
2025-05-20 14:47:37 +0200

    Testing SQLAlchemy connection to Supabase

--- backend/config.py
+++ backend/config.py
@@ -5,13 +5,14 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     stage: Literal['dev', 'prod']
-    postgres_host: str = 'localhost'
+    postgres_host: str = 'https://db.qgqfapwoopmzrkartwfw.supabase.co'
     postgres_user: str = 'postgres'
-    postgres_password: str = 'postgres'
-    postgres_db: str = 'app_db'
+    # postgres_password: str = 'postgres'
+    postgres_password: str = 'vunran-6poswo-gImxos'
+    postgres_db: str = 'postgres'
     postgres_port: str = '5432'
 
-    model_config = SettingsConfigDict(env_file='.env', extra='ignore')
+    model_config = SettingsConfigDict(env_file='.env.example', extra='ignore')
 
 
 settings = Settings()
--- backend/database.py
+++ backend/database.py
@@ -7,8 +7,30 @@ from sqlalchemy.orm import Session, sessionmaker
 
 from .config import settings
 
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.postgres_user}:{settings.postgres_password}@{settings.postgres_host}:{settings.postgres_port}/{settings.postgres_db}'
+# SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.postgres_user}:{settings.postgres_password}@{settings.postgres_host}:{settings.postgres_port}/{settings.postgres_db}'
 
+# SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://{settings.postgres_user}:{settings.postgres_password}@{settings.postgres_host}:{settings.postgres_port}/{settings.postgres_db}?sslmode=require"
+# Database connection parameters
+
+
+POSTGRES_USER = "postgres"
+POSTGRES_PASSWORD = "zusjyx-Hedkyr-wevwi9"  # Replace [YOUR-PASSWORD] with the actual password
+POSTGRES_HOST = "db.qgqfapwoopmzrkartwfw.supabase.co"
+POSTGRES_PORT = "5432"
+POSTGRES_DB = "postgres"
+# user=postgres
+# password=[YOUR-PASSWORD]
+# host=db.qgqfapwoopmzrkartwfw.supabase.co
+# port=5432
+# dbname=postgres
+# SQLAlchemy connection string
+# SQLALCHEMY_DATABASE_URL = (
+#     f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}?sslmode=require"
+# )
+SQLALCHEMY_DATABASE_URL = (
+       "postgresql://postgres.syucjzgfvvmwsnairifl:fawgUs-9vujto-wyxgit@aws-0-eu-central-1.pooler.supabase.com:6543/postgres"
+    )
+print(SQLALCHEMY_DATABASE_URL)
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
@@ -21,3 +43,51 @@ def get_db() -> Generator[Session, Any, None]:
         yield db
     finally:
         db.close()
+
+
+# from sqlalchemy import create_engine, MetaData
+# from sqlalchemy.orm import sessionmaker
+# from sqlalchemy.ext.declarative import declarative_base
+# from dotenv import dotenv_values
+
+# metadata = MetaData()
+# Base = declarative_base()
+
+
+# def db_connect():
+#     # config = dotenv_values("./.env")
+#     username = 'postgres'
+#     password = 'fawgUs-9vujto-wyxgit'
+#     dbname = 'postgres'
+#     port = 5432
+#     host = 'db.syucjzgfvvmwsnairifl.supabase.co' 
+#     engine = create_engine(
+#         "postgresql://postgres.syucjzgfvvmwsnairifl:fawgUs-9vujto-wyxgit@aws-0-eu-central-1.pooler.supabase.com:6543/postgres"
+#     )
+#     connection = engine.connect()
+#     print("Connected to the database")
+#     return engine, connection
+
+
+# def create_tables(engine):
+#     metadata.drop_all(engine, checkfirst=True)
+#     metadata.create_all(engine, checkfirst=True)
+
+
+# def create_tables_orm(engine):
+#     Base.metadata.drop_all(engine, checkfirst=True)
+#     Base.metadata.create_all(engine, checkfirst=True)
+
+
+# def create_session(engine):
+#     Session = sessionmaker(bind=engine)
+#     session = Session()
+#     return session
+
+# if __name__ == '__main__':
+#     # Example usage
+#     engine, conn = db_connect()
+#     session = create_session(engine)
+#     # Example test query
+#     # result = conn.execute('SELECT current_date;')
+#     # print(result.fetchone())
