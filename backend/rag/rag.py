3b0b535f56ae845d22f41570387ea8ab560eff82
Aleksandra Topalova <aleksandra.i.topalova@gmail.com>
2025-05-29 17:52:42 +0300

    feat(B-107): Remove redundancy, remove PGVectorStore

--- backend/rag/rag.py
+++ backend/rag/rag.py
@@ -1,22 +1,55 @@
 import os
 from pathlib import Path
+from typing import Any
 
 from dotenv import load_dotenv
 from embeddings import get_embedding_model
 from langchain.prompts import PromptTemplate
-from langchain_core.runnables import RunnableMap, RunnableSequence
+from langchain_community.vectorstores import SupabaseVectorStore
+from langchain_core.messages import BaseMessage
+from langchain_core.runnables import (
+    RunnableLambda,
+    RunnableSerializable,
+)
+from langchain_core.vectorstores import VectorStoreRetriever
 from langchain_google_genai import ChatGoogleGenerativeAI
-from vectorstore import format_docs, load_vectorstore, prepare_vector_db_docs
+from vector_db import format_docs, load_vector_db, prepare_vector_db_docs
 
 load_dotenv()
 
 BASE_DIR = Path(__file__).parent
 DOC_FOLDER = BASE_DIR / 'documents'
 
-SUPABASE_BACKEND = 'supabase'
 EMBEDDING_TYPE = 'gemini'
 TABLE_NAME = 'hr_information'
 QUERY_FN = 'match_documents'
+SEARCH_TYPE = 'mmr'
+K_SEARCH = 5
+default_prompt = """
+        You are a concise assistant that gives an answer to a query based on the provided context.
+        Context: {context}
+        Query: {query}
+        Answer:
+        """
+
+
+def load_and_index_documents(vector_db: SupabaseVectorStore) -> None:
+    os.makedirs(DOC_FOLDER, exist_ok=True)
+    docs = prepare_vector_db_docs(str(DOC_FOLDER))
+    if not docs:
+        print(f'⚠️ No documents found in folder: {DOC_FOLDER}')
+        return
+
+    vector_db.add_documents(docs)
+    print(f'Added {len(docs)} documents to vector store: {TABLE_NAME}')
+
+
+def get_vector_db_retriever() -> VectorStoreRetriever:
+    embedding = get_embedding_model(EMBEDDING_TYPE)
+    vector_db = load_vector_db(embedding, TABLE_NAME, query_name=QUERY_FN)
+    load_and_index_documents(vector_db)
+    retriever = vector_db.as_retriever(search_type=SEARCH_TYPE, search_kwargs={'k': K_SEARCH})
+    return retriever
 
 
 def get_llm() -> ChatGoogleGenerativeAI:
@@ -25,55 +58,29 @@ def get_llm() -> ChatGoogleGenerativeAI:
     )
 
 
-def load_and_index_documents() -> None:
-    os.makedirs(DOC_FOLDER, exist_ok=True)
-    docs = prepare_vector_db_docs(str(DOC_FOLDER))
-    if not docs:
-        print(f'⚠️ No documents found in folder: {DOC_FOLDER}')
-        return
-
-    embedding = get_embedding_model(EMBEDDING_TYPE)
-    vectorstore = load_vectorstore(SUPABASE_BACKEND, embedding, TABLE_NAME)
-    vectorstore.add_documents(docs)
-    print(f'Added {len(docs)} documents to vector store: {TABLE_NAME}')
-
-
-def rag_chain() -> RunnableSequence:
-    embedding = get_embedding_model(EMBEDDING_TYPE)
-    vectorstore = load_vectorstore(SUPABASE_BACKEND, embedding, TABLE_NAME, query_name=QUERY_FN)
-    retriever = vectorstore.as_retriever(search_type='mmr', search_kwargs={'k': 5})
-
-    prompt = PromptTemplate.from_template(
-        """
-You are a concise assistant that gives an answer to a query based on the provided context.
-Context: {context}
-Query: {query}
-Answer:
-
-    """
-    )
+def rag_chain(prompt: str = default_prompt) -> RunnableSerializable[Any, BaseMessage]:
+    llm = get_llm()
+    retriever = get_vector_db_retriever()
+    prompt = PromptTemplate.from_template(prompt)
 
     return (
-        RunnableMap(
-            {
-                'context': lambda x: format_docs(retriever.invoke(x['query'])),
-                'query': lambda x: x['query'],
-            }
+        RunnableLambda(
+            lambda x: {'context': format_docs(retriever.invoke(x['query'])), 'query': x['query']}
         )
         | prompt
-        | get_llm()
+        | llm
     )
 
 
 if __name__ == '__main__':
-    load_and_index_documents()
+    query = 'What are barriers to effective feedback?'
     chain = rag_chain()
-    result = chain.invoke({'query': 'What are barriers to effective feedback?'})
+    result = chain.invoke({'query': query})
     print(result.content)
     print(
         """
-Sources: 
-Hardavella G, Aamli-Gaagnat A, Saad N, et al. 
-How to give and receive feedback effectively. Breathe 2017; 13: 327–333
-"""
+        Sources: 
+        Hardavella G, Aamli-Gaagnat A, Saad N, et al. 
+        How to give and receive feedback effectively. Breathe 2017; 13: 327–333
+        """
     )
