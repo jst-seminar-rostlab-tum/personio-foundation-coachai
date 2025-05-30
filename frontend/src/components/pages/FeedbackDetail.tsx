c2239bfa5dd16e2e5da6fd85a621f013d3eafe4b
Vishav <ge54nex@mytum.de>
2025-05-30 14:47:19 +0200

    feat(F-114): add training feedback page

---
+++ frontend/src/components/pages/FeedbackDetail.tsx
@@ -0,0 +1,123 @@
+import { useTranslations } from 'next-intl';
+import {
+  CheckCircle,
+  Clock,
+  MessageCircle,
+  MessageCircleQuestion,
+  Mic,
+  PlayIcon,
+  Zap,
+} from 'lucide-react';
+import Progress from '../ui/Progress';
+import { Button } from '../ui/Button';
+
+const mockFeedback = {
+  topic: 'Giving Constructive Feedback',
+  time: '16.04.2025, 12:24',
+  structure: 85,
+  empathy: 85,
+  focus: 85,
+  overall: 82,
+  speakingTime: 62,
+  questionAsked: 6,
+  sessionLength: '3:30',
+  goalsAcheived: '4/5',
+  conversationLength: 3,
+};
+
+export default function FeedbackDetail() {
+  const t = useTranslations('Feedback');
+  const progressBarData = [
+    { key: t('progressBars.structure'), value: mockFeedback.structure },
+    { key: t('progressBars.empathy'), value: mockFeedback.empathy },
+    { key: t('progressBars.focus'), value: mockFeedback.focus },
+  ];
+
+  const roundCardStats = [
+    { key: t('stats.speakingTime'), value: `${mockFeedback.speakingTime}%`, icon: 'Mic' },
+    { key: t('stats.questionsAsked'), value: mockFeedback.questionAsked, icon: 'Message' },
+    { key: t('stats.sessionLength'), value: mockFeedback.sessionLength, icon: 'Clock' },
+    { key: t('stats.goalsAchieved'), value: mockFeedback.goalsAcheived, icon: 'Check' },
+  ];
+
+  const getIcon = (iconName: string, size: number) => {
+    switch (iconName) {
+      case 'Mic':
+        return <Mic size={size} />;
+      case 'Message':
+        return <MessageCircle size={size} />;
+      case 'Clock':
+        return <Clock size={size} />;
+      case 'Check':
+        return <CheckCircle size={size} />;
+      default:
+        return <MessageCircleQuestion size={size} />;
+    }
+  };
+  return (
+    <div className="flex flex-col items-center gap-5 mx-auto md:w-150">
+      <div className="text-2xl ">{t('title')}</div>
+      <div className="h-20 bg-marigold-10 px-4 py-5 rounded-md text-center w-full">
+        <div className="text-lg">{mockFeedback.topic}</div>
+        <div className="text-base">{mockFeedback.time}</div>
+      </div>
+      <div className="flex gap-3 items-center w-full justify-between">
+        <div className="flex flex-col gap-4 p-2.5 flex-1">
+          {progressBarData.map((item) => (
+            <div key={item.key} className="flex flex-col gap-2">
+              <div className="flex justify-between text-base">
+                <span>{item.key}</span>
+                <span>{item.value}%</span>
+              </div>
+              <Progress className="w-full" value={item.value} />
+            </div>
+          ))}
+        </div>
+        <div className="size-25 rounded-full bg-marigold-10 flex items-center justify-center">
+          {mockFeedback.overall}%
+        </div>
+      </div>
+      <div className="my-4 mx-2 h-px w-full bg-bw-30" />
+
+      <div className="grid grid-cols-2 gap-y-4 w-full px-1">
+        {roundCardStats.map((stat) => (
+          <div className="flex gap-2 items-center md:justify-center" key={stat.key}>
+            <div className="rounded-full size-11 border-1 border-bw-30 bg-bw-10 flex items-center justify-center">
+              {getIcon(stat.icon, 20)}
+            </div>
+            <div className="flex flex-col gap-1 justify-between">
+              <span className="text-base">{stat.key}</span>
+              <span className="text-md">{stat.value}</span>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="my-4 mx-2 h-px w-full bg-bw-30" />
+      <div className="flex items-center justify-center gap-3 mx-1 px-3 w-full h-20 bg-bw-10 rounded-md">
+        <div className="size-11 rounded-full bg-marigold-50 flex items-center justify-center">
+          <PlayIcon size={20} className="text-white" />
+        </div>
+        <div className="flex flex-col justify-between flex-1">
+          <span className="text-md">{t('listenConversation')}</span>
+          <div className="flex gap-3 items-center">
+            <Progress className="w-10 flex-1" value={62} />
+            <div className="flex gap-1 items-center text-base text-bw-40">
+              <Clock size={13} />
+              <span>{mockFeedback.conversationLength} min</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex gap-3 w-full">
+        <Button variant="outline" size="full">
+          {t('yourApproach')}
+        </Button>
+        <Button size="full">
+          <Zap size={13} />
+          {t('compareAI')}
+        </Button>
+      </div>
+      <Button size="full">{t('return')}</Button>
+    </div>
+  );
+}
