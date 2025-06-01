ee3274ab04a7d3f4fadd2621f2124487ddee0460
Simon Paul <simon.paul@tum.de>
2025-06-01 19:32:46 +0200

    feat(B-130): Fixed env loading

--- backend/rag/embeddings.py
+++ backend/rag/embeddings.py
@@ -1,14 +1,16 @@
-import os
-
 from langchain.embeddings.base import Embeddings
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_huggingface import HuggingFaceEmbeddings
+
+from backend.config import Settings
+
+settings = Settings()
 
 
 def get_embedding_model(model_type: str = 'huggingface') -> Embeddings:
     if model_type == 'gemini':
         return GoogleGenerativeAIEmbeddings(
-            model='models/embedding-001', google_api_key=os.getenv('GEMINI_API_KEY')
+            model='models/embedding-001', google_api_key=settings.GEMINI_API_KEY
         )
     elif model_type == 'huggingface':
         return HuggingFaceEmbeddings(model_name='sentence-transformers/all-mpnet-base-v2')
