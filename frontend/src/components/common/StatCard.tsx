9de43dc9769ca764bdab67b0842dc6554757fcec
Sam Miao <miao.ymg@gmail.com>
2025-06-02 22:11:13 +0200

    feat(F-59): update usage of accent font across the application

--- frontend/src/components/common/StatCard.tsx
+++ frontend/src/components/common/StatCard.tsx
@@ -1,15 +1,12 @@
 'use client';
 
 import { StatCardProps } from '@/interfaces/StatCardProps';
-import { Bebas_Neue as BebasNeue } from 'next/font/google';
-
-const bebasNeue = BebasNeue({ subsets: ['latin'], weight: '400' });
 
 export default function StatCard({ value, label }: StatCardProps) {
   return (
     <div className="@container w-full bg-bw-10 rounded-lg flex flex-col justify-center gap-2 p-4 md:p-6">
       <p className="text-base md:text-lg text-bw-40">{label}</p>
-      <p className={`${bebasNeue.className} text-4xl md:text-6xl`}>{value}</p>
+      <p className="bebas-neue text-4xl md:text-6xl">{value}</p>
     </div>
   );
 }
