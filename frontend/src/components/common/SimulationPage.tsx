9d9b5bd539ea7936db47fd79fae6aa9fe0dfede1
Christopher Lyko <christopher.lyko@tum.de>
2025-06-02 18:24:39 +0200

    feat(B-137): implement mic toggle button

--- frontend/src/components/common/SimulationPage.tsx
+++ frontend/src/components/common/SimulationPage.tsx
@@ -316,6 +316,7 @@ export default function SimulationPageComponent() {
     if (localStreamRef.current) {
       localStreamRef.current.getAudioTracks().forEach((track) => {
         track.enabled = !isMicActive;
+        console.log("test";)
       });
       setIsMicActive(!isMicActive);
     }
