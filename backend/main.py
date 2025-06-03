5f38e95985270e4318bf7480662686348c00fa99
thinkinbig <56369697+thinkinbig@users.noreply.github.com>
2025-06-03 13:10:38 +0200

    Revert "Feat/webrtc"

--- backend/main.py
+++ backend/main.py
@@ -23,11 +23,9 @@ from .routers import (
     user_confidence_score_route,
     user_goals_route,
     user_profile_route,
-    webrtc_signal_route,
 )
 
 app = FastAPI(title='CoachAI', debug=settings.stage == 'dev')
-
 
 app.add_middleware(
     CORSMiddleware,
@@ -54,7 +52,6 @@ app.include_router(learning_style_route.router)
 app.include_router(session_length_route.router)
 app.include_router(personalization_options_route.router)
 app.include_router(app_config_route.router)
-app.include_router(webrtc_signal_route.router)
 
 
 # Create database tables on startup
