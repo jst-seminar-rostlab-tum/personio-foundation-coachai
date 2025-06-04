d57fe611ecbc510f2d6231ef4e70875953606725
Yulius Seliman <yulius@byintention.de>
2025-06-04 14:06:09 +0200

    fix(M): fix type error for Metadata

--- frontend/src/app/[locale]/(standalone)/simulation/[id]/page.tsx
+++ frontend/src/app/[locale]/(standalone)/simulation/[id]/page.tsx
@@ -1,9 +1,9 @@
 import SimulationPageComponent from '@/components/common/SimulationPage';
 import { generateMetadata as generateDynamicMetadata } from '@/lib/metadata';
 import type { Metadata } from 'next';
-import type { Props } from '@/interfaces/LayoutProps';
+import { MetadataProps } from '@/interfaces/MetadataProps';
 
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
+export async function generateMetadata({ params }: MetadataProps): Promise<Metadata> {
   const { locale } = await params;
   return generateDynamicMetadata(locale, '/simulation/[id]', true);
 }
