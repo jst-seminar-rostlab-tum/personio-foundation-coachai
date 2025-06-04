cf041f7c1910800f93451376468f5a7cba39db31
Anna Schleich <anna.schleich@outlook.de>
2025-06-04 20:18:21 +0200

    fix(M): fix import path

--- backend/app/models/training_session.py
+++ backend/app/models/training_session.py
@@ -7,7 +7,7 @@ from sqlalchemy.engine.base import Connection
 from sqlalchemy.orm.mapper import Mapper
 from sqlmodel import JSON, Column, Field, Relationship, SQLModel
 
-from backend.models.training_session_feedback import TrainingSessionFeedbackMetrics
+from app.models.training_session_feedback import TrainingSessionFeedbackMetrics
 
 if TYPE_CHECKING:
     from app.models.conversation_turn import ConversationTurn
@@ -20,9 +20,9 @@ if TYPE_CHECKING:
 class TrainingSession(SQLModel, table=True):  # `table=True` makes it a database table
     id: UUID = Field(default_factory=uuid4, primary_key=True)
     case_id: UUID = Field(foreign_key='trainingcase.id')  # Foreign key to TrainingCase
-    scheduled_at: Optional[datetime] = None
-    started_at: Optional[datetime] = None
-    ended_at: Optional[datetime] = None
+    scheduled_at: datetime | None = None
+    started_at: datetime | None = None
+    ended_at: datetime | None = None
     language_code: str = Field(foreign_key='language.code')  # Foreign key to LanguageModel
     ai_persona: dict = Field(default_factory=dict, sa_column=Column(JSON))
     created_at: datetime = Field(default_factory=datetime.utcnow)
@@ -50,9 +50,9 @@ def update_timestamp(mapper: Mapper, connection: Connection, target: 'TrainingSe
 # Schema for creating a new TrainingSession
 class TrainingSessionCreate(SQLModel):
     case_id: UUID
-    scheduled_at: Optional[datetime] = None
-    started_at: Optional[datetime] = None
-    ended_at: Optional[datetime] = None
+    scheduled_at: datetime | None = None
+    started_at: datetime | None = None
+    ended_at: datetime | None = None
     language_code: str
     ai_persona: dict = Field(default_factory=dict, sa_column=Column(JSON))
 
@@ -61,9 +61,9 @@ class TrainingSessionCreate(SQLModel):
 class TrainingSessionRead(SQLModel):
     id: UUID
     case_id: UUID
-    scheduled_at: Optional[datetime]
-    started_at: Optional[datetime]
-    ended_at: Optional[datetime]
+    scheduled_at: datetime | None
+    started_at: datetime | None
+    ended_at: datetime | None
     language_code: str
     ai_persona: dict = Field(default_factory=dict, sa_column=Column(JSON))
     created_at: datetime
@@ -73,8 +73,8 @@ class TrainingSessionRead(SQLModel):
 # Schema for reading TrainingSession data with details including skill scores, goals achieved,
 # session metrics, and feedback insights
 class TrainingSessionDetailsRead(TrainingSessionRead):
-    title: Optional[str] = None
-    summary: Optional[str] = None
+    title: str | None = None
+    summary: str | None = None
     feedback: Optional['TrainingSessionFeedbackMetrics'] = None
     # List of audio file URIs --> located in conversation_turns
     audio_uris: list[str] = Field(default_factory=list)
