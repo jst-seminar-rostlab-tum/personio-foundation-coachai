91928a45f0cf762ae089c776414f093d778541e7
Vishav <ge54nex@mytum.de>
2025-06-06 16:09:52 +0200

    feat(F-132): show stats in 2 lines

--- frontend/src/app/[locale]/(main)/history/components/HistoryStats.tsx
+++ frontend/src/app/[locale]/(main)/history/components/HistoryStats.tsx
@@ -15,7 +15,7 @@ export default function HistoryStats() {
       <div className="w-full flex flex-col  gap-6">
         <div className="flex-1">
           <div className="text-xl mb-4 text-bw-70">{t('skillsPerformance')}</div>
-          <div className="flex flex-col gap-6 lg:flex-row lg:gap-8 px-2">
+          <div className="flex flex-col gap-6 lg:grid lg:grid-cols-2 lg:gap-y-8 px-2">
             {mockStats.map((stat) => (
               <div key={stat.key} className="flex-1">
                 <div className="flex items-center justify-between mb-1">
