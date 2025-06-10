afb5600644b5d1af4f330261bb18a80a390fbbd2
Feres Essafi <feres.essafi.1@gmail.com>
2025-06-10 20:30:03 +0200

    chore(F-211): revert Api.ts service

--- frontend/src/services/Api.ts
+++ frontend/src/services/Api.ts
@@ -14,10 +14,10 @@ const api = axios.create({
 // Request interceptor for adding auth token
 api.interceptors.request.use(
   (config) => {
-    // const token = localStorage.getItem('token');
-    // if (token) {
-    //  config.headers?.set?.('Authorization', `Bearer ${token}`);
-    // }
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers?.set?.('Authorization', `Bearer ${token}`);
+    }
     return config;
   },
   (error) => Promise.reject(error)
