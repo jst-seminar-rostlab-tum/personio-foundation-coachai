04672d855ce4e8a708ea70f73ea23d350c6fa757
Stefan Dimitrov <svdimitrv@gmail.com>
2025-06-11 23:35:35 +0200

    feat(M): Update env variable names

--- backend/app/config.py
+++ backend/app/config.py
@@ -16,7 +16,7 @@ class Settings(BaseSettings):
     database_url: str | None = None
 
     SUPABASE_URL: str = ''
-    SUPABASE_KEY: str = ''
+    SUPABASE_ANON_KEY: str = ''
     SUPABASE_SERVICE_ROLE_KEY: str = ''
     SUPABASE_JWT_SECRET: str = ''
 
