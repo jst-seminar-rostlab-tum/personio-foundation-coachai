98b937660b55eeb8f5283ed4d80c2daca369f497
TA <ta.meyerhoff@protonmail.com>
2025-06-12 23:31:16 +0200

    fix(M): disable authentication middleware by default

--- frontend/env.d.ts
+++ frontend/env.d.ts
@@ -2,7 +2,7 @@ declare namespace NodeJS {
   interface ProcessEnv {
     NEXT_PUBLIC_SUPABASE_URL: string;
     NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
-    DEV_MODE_SKIP_AUTH: boolean;
+    DEV_MODE_SKIP_AUTH: boolean; // not in use atm
     NEXT_PUBLIC_API_URL: string; // defaults to http://localhost:8000
     NEXT_PUBLIC_BASE_URL: string; // defaults to https://coachai-personio-foundation.vercel.app
   }
