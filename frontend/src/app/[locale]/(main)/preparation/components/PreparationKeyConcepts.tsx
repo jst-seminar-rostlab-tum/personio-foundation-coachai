03dcb2bb604aea65404921e03633a72f17bd0d5a
Sam Miao <107631268+miao-ymg@users.noreply.github.com>
2025-06-14 16:55:26 +0200

    feat(F-212): integrate with get training case preparation endpoint (#304)
    * feat(F-212): integrate with get training case preparation endpoint
    
    * feat(F-212): display loading screen for status code 202
    
    * fix(F-212): fix error with infinite API requests
    
    * feat(F-212): create Prepartion integration
    
    * fix(M): exclude authentication for development in the frontend
    
    ---------
    
    Co-authored-by: Yulius Seliman <yulius@byintention.de>
    Co-authored-by: Christopher Lyko <christopher.lyko@tum.de>

---
+++ frontend/src/app/[locale]/(main)/preparation/components/PreparationKeyConcepts.tsx
@@ -0,0 +1,19 @@
+import { KeyConcept } from '@/interfaces/TrainingPreparation';
+
+export default function PreparationKeyConcepts({ keyConcepts }: { keyConcepts: KeyConcept[] }) {
+  return (
+    <div className="space-y-4">
+      {keyConcepts.map((concept, index) => (
+        <div key={index} className="flex flex-col gap-2">
+          <label
+            htmlFor={`check-${index}`}
+            className="text-lg font-bold text-bw-70 leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+          >
+            {concept.header}
+          </label>
+          <p className="text-base text-bw-70">{concept.value}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
