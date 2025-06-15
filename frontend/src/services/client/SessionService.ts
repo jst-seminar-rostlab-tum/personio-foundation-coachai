465f8cb21d48122eedb90e3d13fe9555a8917de6
JCaesar20 <60732005+JCaesar20@users.noreply.github.com>
2025-06-15 17:59:48 +0200

    chore(M): Clean Frontend Services
    Co-authored-by: Yulius Seliman <yulius@byintention.de>

--- frontend/src/services/client/SessionService.ts
+++ frontend/src/services/client/SessionService.ts
@@ -1,3 +1,4 @@
+import { Session } from '@/interfaces/Session';
 import { api } from './Api';
 
 export const getSessionFeedback = async (sessionId: string) => {
@@ -20,6 +21,31 @@ export const clearAllSessions = async () => {
   }
 };
 
-export const SessionService = {
+const createSession = async (scenarioId: string) => {
+  try {
+    const response = await api.post<Session>('/session/', {
+      scenarioId,
+    });
+    return response;
+  } catch (error) {
+    console.error('Error creating conversation scenario:', error);
+    throw error;
+  }
+};
+
+const updateSession = async (sessionId: string, session: Partial<Session>) => {
+  try {
+    const response = await api.put<Session>(`/session/${sessionId}/`, session);
+    return response;
+  } catch (error) {
+    console.error('Error updating session:', error);
+    throw error;
+  }
+};
+
+export const sessionService = {
   clearAllSessions,
+  createSession,
+  updateSession,
+  getSessionFeedback,
 };
