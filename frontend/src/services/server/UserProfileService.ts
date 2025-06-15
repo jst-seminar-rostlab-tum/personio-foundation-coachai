f1178a98ff51ea9ac8f3f6e2710182dbe75b5111
Vishav <ge54nex@mytum.de>
2025-06-15 18:05:25 +0200

    fix(M): remove update from server service

--- frontend/src/services/server/UserProfileService.ts
+++ frontend/src/services/server/UserProfileService.ts
@@ -13,17 +13,6 @@ const getUserProfile = async (): Promise<UserProfile> => {
   }
 };
 
-const updateUserProfile = async (userProfile: UserProfile): Promise<UserProfile> => {
-  try {
-    const { data } = await api.patch<UserProfile>('/user-profile', userProfile);
-    return data;
-  } catch (error) {
-    console.error('Error updating user profile:', error);
-    throw error;
-  }
-};
-
 export const UserProfileService = {
   getUserProfile,
-  updateUserProfile,
 };
