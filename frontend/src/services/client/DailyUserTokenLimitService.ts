005cacb55b04d36ca6fd529d2dfca5fd056f74f3
Teodora Petrova <t.petrova1234@gmail.com>
2025-06-15 22:15:02 +0200

    feat(F-274): Adjustments

--- frontend/src/services/client/DailyUserTokenLimitService.ts
+++ frontend/src/services/client/DailyUserTokenLimitService.ts
@@ -1,8 +1,13 @@
 import { api } from './Api';
 
 export const updateDailyUserTokenLimit = async (limit: number) => {
-  const { data } = await api.put(`/app-config`, {
-    dailyUserTokenLimit: limit,
-  });
-  return data;
+  try {
+    const { data } = await api.put(`/app-config`, {
+      dailyUserTokenLimit: limit,
+    });
+    return data;
+  } catch (error) {
+    console.error('Error updating daily user token limit: ', error);
+    throw error;
+  }
 };
