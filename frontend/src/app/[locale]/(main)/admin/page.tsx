0290c0f8e4e21c19b151b0f08e7be90fb7712dc4
Stefan Dimitrov <svdimitrv@gmail.com>
2025-06-17 00:49:19 +0200

    fix(M): Redirect to dashboard if regular user attempts to access the admin page

--- frontend/src/app/[locale]/(main)/admin/page.tsx
+++ frontend/src/app/[locale]/(main)/admin/page.tsx
@@ -2,6 +2,9 @@ import { generateMetadata as generateDynamicMetadata } from '@/lib/metadata';
 import type { Metadata } from 'next';
 import { MetadataProps } from '@/interfaces/MetadataProps';
 import { adminService } from '@/services/server/AdminService';
+import { UserProfileService } from '@/services/server/UserProfileService';
+import { AccountRole } from '@/interfaces/UserProfile';
+import { redirect } from 'next/navigation';
 import Admin from './components/AdminPage';
 
 export async function generateMetadata({ params }: MetadataProps): Promise<Metadata> {
@@ -9,7 +12,12 @@ export async function generateMetadata({ params }: MetadataProps): Promise<Metad
   return generateDynamicMetadata(locale, '/admin', true);
 }
 
-export default function AdminPage() {
-  const stats = adminService.getAdminStats();
+export default async function AdminPage() {
+  const userProfile = await UserProfileService.getUserProfile();
+  if (userProfile.accountRole !== AccountRole.admin) {
+    return redirect('/dashboard');
+  }
+
+  const stats = await adminService.getAdminStats();
   return <Admin stats={stats} />;
 }
