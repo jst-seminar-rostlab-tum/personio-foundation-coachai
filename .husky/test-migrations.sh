3d504ebfa53960b769538479d93ea3696db5b45e
Christopher Lyko <christopher.lyko@tum.de>
2025-06-19 10:55:17 +0200

    remove special characters from test-migrations.sh

--- .husky/test-migrations.sh
+++ .husky/test-migrations.sh
@@ -1,12 +1,13 @@
+#!/bin/bash
 set -e
 
 psql -h db -U postgres -tAc "SELECT 1 FROM pg_database WHERE datname='test_migrations'" | grep -q 1 || \
 psql -h db -U postgres -c "CREATE DATABASE test_migrations;"
 
 HEADS=$(uv run alembic heads | wc -l)
-if [ "$HEADS" -gt 1 ]; then 
-    echo "❌- Multiple Alembic heads detected! Run 'uv run alembic heads'."; 
-    exit 1; 
+if [ "$HEADS" -gt 1 ]; then
+    echo "Multiple Alembic heads detected! Run 'uv run alembic heads'.";
+    exit 1;
 fi
 
 uv run -m app.data.populate_dummy_data
