a8f5f69853e998709e36af7251effce9322a6c16
Christopher Lyko <christopher.lyko@tum.de>
2025-06-19 15:50:56 +0200

    feat(M): add seed data for conversation category table

--- .husky/test-migrations.sh
+++ .husky/test-migrations.sh
@@ -10,9 +10,9 @@ fi
 psql -h db -U postgres -tAc "SELECT 1 FROM pg_database WHERE datname='test_migrations'" | grep -q 1 || \
 psql -h db -U postgres -c "CREATE DATABASE test_migrations;"
 
-uv run -m app.data.populate_dummy_data
+uv run alembic downgrade base
+uv run alembic upgrade head
 uv run alembic check
+uv run alembic downgrade base
+uv run alembic upgrade head
 uv run -m app.data.populate_dummy_data
-
-echo "DONE RUNNING test-migrations.sh"
-
