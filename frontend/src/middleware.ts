b3654e3d98ef0c25a2b1bdbe9a91a1abcd830d6e
Feres Essafi <feres.essafi.1@gmail.com>
2025-06-27 11:43:40 +0200

    feat(F-533): add supabase to csp

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -7,9 +7,10 @@ const i18nMiddleware = createMiddleware(routing);
 
 export default async function middleware(request: NextRequest) {
   const nonce = Buffer.from(crypto.randomUUID()).toString('base64');
+
   const cspHeader = `
     default-src 'self';
-    connect-src 'self' ${process.env.NODE_ENV === 'production' ? '' : `${process.env.NEXT_PUBLIC_API_URL}`};
+    connect-src 'self' ${process.env.NEXT_PUBLIC_SUPABASE_URL} ${process.env.NODE_ENV === 'production' ? '' : `${process.env.NEXT_PUBLIC_API_URL}`};
     script-src 'self' 'nonce-${nonce}' 'strict-dynamic' ${process.env.NODE_ENV === 'production' ? '' : `'unsafe-eval'`};
     style-src 'self' 'unsafe-inline';
     img-src 'self' blob: data:;
@@ -20,35 +21,20 @@ export default async function middleware(request: NextRequest) {
     frame-ancestors 'none';
     upgrade-insecure-requests;
 `;
-  // Replace newline characters and spaces
+
   const contentSecurityPolicyHeaderValue = cspHeader.replace(/\s{2,}/g, ' ').trim();
-
   const response = i18nMiddleware(request);
-
   response.headers.set('x-nonce', nonce);
   response.headers.set('Content-Security-Policy', contentSecurityPolicyHeaderValue);
 
-  // Skip authentication if in development mode and the environment variable is set
   if (process.env.NODE_ENV === 'development') {
     return response;
   }
-
   return authMiddleware(request, response);
 }
 
 export const config = {
-  // Match all pathnames except for
-  // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
-  // - … the ones containing a dot (e.g. `favicon.ico`)
-  // - … the ones containing a dot (e.g. `favicon.ico`)
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
     {
       source: '/((?!api|_next|_vercel|.*\\.[^/]+$).*)',
       missing: [
