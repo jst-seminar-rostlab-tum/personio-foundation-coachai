ad5d1666cbe5874e12bf62000afe8862e7699255
Feres Essafi <feres.essafi.1@gmail.com>
2025-06-27 23:30:24 +0200

    fix(F-533): fix cspHeader connect-src

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -10,7 +10,7 @@ export default async function middleware(request: NextRequest) {
 
   const cspHeader = `
     default-src 'self';
-    connect-src 'self' ${process.env.NEXT_PUBLIC_SUPABASE_URL} ${process.env.NODE_ENV === 'production' ? '' : `${process.env.NEXT_PUBLIC_API_URL}`};
+    connect-src 'self' ${process.env.NEXT_PUBLIC_SUPABASE_URL} ${process.env.NEXT_PUBLIC_API_URL};
     script-src 'self' 'nonce-${nonce}' 'strict-dynamic' ${process.env.NODE_ENV === 'production' ? '' : `'unsafe-eval'`};
     style-src 'self' 'unsafe-inline';
     img-src 'self' blob: data:;
