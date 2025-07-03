17a12bf519430beb9861847a30eea06be571b753
Simon Paul <simon.paul@tum.de>
2025-07-03 17:11:15 +0200

    feat(M): Commented out skip jwt logic since it doesn't work

--- backend/app/dependencies.py
+++ backend/app/dependencies.py
@@ -38,10 +38,10 @@ def verify_jwt(
     """
     Checks the validity of the JWT token and retrieves its information.
     """
+    # if settings.stage == 'dev' and settings.DEV_MODE_SKIP_AUTH:
+    #   logging.info('Skipping JWT verification')
+    #   return JWTPayload(sub=str(settings.DEV_MODE_MOCK_USER_ID))
     logging.info('Verifying JWT')
-    if settings.stage == 'dev' and settings.DEV_MODE_SKIP_AUTH:
-        logging.info('Skipping JWT verification')
-        return {'sub': str(settings.DEV_MODE_MOCK_USER_ID)}
     if not credentials:
         logging.info('No JWT token')
         raise HTTPException(
