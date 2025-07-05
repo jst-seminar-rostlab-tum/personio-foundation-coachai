27d1f9aff06cd6d153e022414489e18884a5b9d9
Fatairy <elfatairyomar@gmail.com>
2025-07-05 15:50:28 +0200

    lint fix

--- backend/alembic/versions/2025_07_05_1547-6228e2d29828_merged_alembic_head.py
+++ backend/alembic/versions/2025_07_05_1547-6228e2d29828_merged_alembic_head.py
@@ -9,11 +9,6 @@ Create Date: 2025-07-05 15:47:27.450035
 from collections.abc import Sequence
 from typing import Union
 
-import sqlalchemy as sa
-import sqlmodel
-
-from alembic import op
-
 # revision identifiers, used by Alembic.
 revision: str = '6228e2d29828'
 down_revision: Union[str, None] = '8b0f8f5f8fc1'
