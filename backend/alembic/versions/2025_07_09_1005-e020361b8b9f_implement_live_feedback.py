12985df935d15cfefcd34176203459ac54fa3b1a
Aleksandra Topalova <aleksandra.i.topalova@gmail.com>
2025-07-09 14:21:32 +0200

    feat(B-484): Resolve multiple heads alembic

--- backend/alembic/versions/2025_07_09_1005-e020361b8b9f_implement_live_feedback.py
+++ backend/alembic/versions/2025_07_09_1005-e020361b8b9f_implement_live_feedback.py
@@ -15,7 +15,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = 'e020361b8b9f'
-down_revision: Union[str, None] = 'b2117ff73279'
+down_revision: Union[str, None] = '03f53ff9f076'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
