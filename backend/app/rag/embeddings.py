8152b9dbca822c4ff99ae792433fc7cfd1dcae88
Aleksandra Topalova <aleksandra.i.topalova@gmail.com>
2025-07-09 16:22:57 +0200

    Create vertex ai client (#669)
    * feat(B-625): VertexAI client base
    
    * feat(B-625): Allow to call llm with audio, adapt config
    
    * feat(B-625): Allow to call llm with audio, adapt config
    
    * feat(B-625): Allow to call llm with audio, adapt config
    
    * feat(B-625): Allow to call llm with audio, adapt config
    
    * feat(B-625): State explicitly response mime type, add error handling
    
    * feat(B-625): Finish structured output generation with vertex ai and substitute in services
    
    * feat(B-625): Adjust voice analysis and vector db code and its tests
    
    * feat(B-625): Adjust config to vertexai project id
    
    * feat(B-625): Add vertex ai embedding model to rag pipeline
    
    * feat(B-625): VertexAI client base
    
    * feat(B-625): Allow to call llm with audio, adapt config
    
    * feat(B-625): Allow to call llm with audio, adapt config
    
    * feat(B-625): Allow to call llm with audio, adapt config
    
    * feat(B-625): Allow to call llm with audio, adapt config
    
    * feat(B-625): State explicitly response mime type, add error handling
    
    * feat(B-625): Finish structured output generation with vertex ai and substitute in services
    
    * feat(B-625): Adjust voice analysis and vector db code and its tests
    
    * feat(B-625): Adjust config to vertexai project id
    
    * feat(B-625): Add vertex ai embedding model to rag pipeline
    
    * feat(B-625): Add vertex ai call in session feedback
    
    * feat(B-625): Fix vertex ai call in session feedback, remove model
    
    * feat(B-625): Adapt README and push function match_documents to local db
    
    * feat(B-625): Update toml and uv files
    
    * feat(B-625): Fix client cred
    
    * feat(B-625): Fix client cred
    
    * feat(B-625): Unify gcp and vertexai credentials, check voice analysis path differently
    
    * feat(B-625): Update populate vector db to use vertexai, readme and info messages in vertexai client

--- backend/app/rag/embeddings.py
+++ backend/app/rag/embeddings.py
@@ -1,14 +1,16 @@
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_google_vertexai import VertexAIEmbeddings
 from langchain_openai import OpenAIEmbeddings
 
 from app.config import Settings
+from app.connections.vertexai_client import credentials
 
 settings = Settings()
 
 
 def get_embedding_model(
-    model_type: str = 'gemini',
-) -> GoogleGenerativeAIEmbeddings | OpenAIEmbeddings:
+    model_type: str = 'vertexai',
+) -> VertexAIEmbeddings | GoogleGenerativeAIEmbeddings | OpenAIEmbeddings:
     """
     Returns an embedding model instance based on the specified provider.
 
@@ -28,9 +30,12 @@ def get_embedding_model(
     Raises:
         ValueError: If an unsupported model type is specified.
     """
-    if model_type == 'gemini':
+    if model_type == 'vertexai':
+        if not credentials:
+            raise ValueError('No GEMINI_API_KEY to create embedding model')
+        return VertexAIEmbeddings(model_name='text-embedding-004', credentials=credentials)
+    elif model_type == 'gemini':
         if not settings.GEMINI_API_KEY:
-            print('No GEMINI_API_KEY to create embedding model')
             raise ValueError('No GEMINI_API_KEY to create embedding model')
         return GoogleGenerativeAIEmbeddings(
             model='models/embedding-001', google_api_key=settings.GEMINI_API_KEY
