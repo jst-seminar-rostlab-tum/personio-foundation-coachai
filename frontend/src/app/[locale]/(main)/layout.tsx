5268fc8d1f559294d609dc94f290d7ca8e7975fe
Yulius Seliman <yulius@byintention.de>
2025-07-11 14:18:48 +0200

    refactor(M): improve user profile fetching and context management

--- frontend/src/app/[locale]/(main)/layout.tsx
+++ frontend/src/app/[locale]/(main)/layout.tsx
@@ -4,17 +4,12 @@ import { LayoutProps } from '@/interfaces/props/LayoutProps';
 import { AppHeader } from '@/components/layout/AppHeader';
 import BackButton from '@/components/common/BackButton';
 import { Toaster } from '@/components/ui/Sonner';
-
 import { UserContextProvider } from '@/contexts/User';
-import { UserProfileService } from '@/services/UserProfileService';
-import { api } from '@/services/ApiServer';
 
 export default async function MainLayout({ children }: LayoutProps) {
-  const userProfile = await UserProfileService.getUserProfile(api);
-
   return (
     <NextIntlClientProvider>
-      <UserContextProvider user={userProfile}>
+      <UserContextProvider>
         <AppHeader />
         <main className="mx-auto py-12 px-[clamp(1.25rem,4vw,4rem)] max-w-7xl mt-16">
           <BackButton />
