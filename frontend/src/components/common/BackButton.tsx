151bcdbab47559806c789c07428be16a85b20231
Yulius Seliman <yulius@byintention.de>
2025-07-24 16:21:37 +0200

    feat(M): Improve FeedbackDetail component with BackButton and date formatting logic for safari

--- frontend/src/components/common/BackButton.tsx
+++ frontend/src/components/common/BackButton.tsx
@@ -7,26 +7,35 @@ import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import { useTranslations } from 'next-intl';
 
-export default function BackButton() {
-  const t = useTranslations('Common');
-  const pathname = usePathname();
+export const BackButtonComponent = ({ label }: { label: string }) => {
+  return (
+    <Button
+      variant="ghost"
+      className={cn(
+        'group relative flex items-center gap-2 bg-transparent mb-8 p-0 text-xl text-bw-40 transition-colors duration-200 hover:text-bw-70 hover:bg-transparent'
+      )}
+    >
+      <ArrowLeftIcon className="h-4 w-4" strokeWidth={3} />
+      <span className="relative transition-transform duration-200 group-hover:-translate-x-1">
+        {label}
+      </span>
+    </Button>
+  );
+};
 
-  if (pathname.includes('/dashboard') || pathname.includes('/preparation')) {
+export default function BackButton() {
+  const pathname = usePathname();
+  const t = useTranslations('Common');
+  if (
+    pathname.includes('/dashboard') ||
+    pathname.includes('/preparation') ||
+    pathname.includes('/feedback')
+  ) {
     return null;
   }
   return (
     <Link href="/dashboard" className="block w-fit">
-      <Button
-        variant="ghost"
-        className={cn(
-          'group relative flex items-center gap-2 bg-transparent mb-8 p-0 text-xl text-bw-40 transition-colors duration-200 hover:text-bw-70 hover:bg-transparent'
-        )}
-      >
-        <ArrowLeftIcon className="h-4 w-4" strokeWidth={3} />
-        <span className="relative transition-transform duration-200 group-hover:-translate-x-1">
-          {t('backToHome')}
-        </span>
-      </Button>
+      <BackButtonComponent label={t('backToHome')} />
     </Link>
   );
 }
