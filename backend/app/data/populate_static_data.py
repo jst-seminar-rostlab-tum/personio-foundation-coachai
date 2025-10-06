5247ada5f8cb7d70c883600f7462cee8ca06f519
Christopher Lyko <christopher.lyko@tum.de>
2025-10-07 00:16:07 +0200

    fix(M): fixed pagination for user list in admin view

--- backend/app/data/populate_static_data.py
+++ backend/app/data/populate_static_data.py
@@ -2,7 +2,6 @@ import json
 import os
 from datetime import UTC, datetime
 
-from database import get_supabase_client
 from sqlmodel import Session as DBSession
 from sqlmodel import delete, select
 
@@ -10,7 +9,7 @@ from app.data import (
     get_dummy_app_configs,
 )
 from app.data.dummy_data import get_dummy_user_data
-from app.database import engine
+from app.database import engine, get_supabase_client
 from app.enums.language import LanguageCode
 from app.models import AppConfig, ConversationCategory, UserProfile
 
