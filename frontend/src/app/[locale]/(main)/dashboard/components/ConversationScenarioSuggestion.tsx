66a1d48d1d5a01b4c9d8fbee6bada48ef4924974
Yulius Seliman <yulius@byintention.de>
2025-10-26 22:02:34 +0100

    style(M): update border colors in ConversationScenarioSuggestion for improved UI consistency

--- frontend/src/app/[locale]/(main)/dashboard/components/ConversationScenarioSuggestion.tsx
+++ frontend/src/app/[locale]/(main)/dashboard/components/ConversationScenarioSuggestion.tsx
@@ -51,7 +51,7 @@ export default function ConversationScenarioSuggestion({
     <section className="flex flex-col items-center justify-center w-full">
       <div className="flex flex-col md:flex-row md:items-center gap-6 mb-4 justify-center w-full max-w-2xl">
         <div
-          className={`relative p-8 max-w-lg break-words border rounded-lg border-bw-50 bg-custom-beige mx-auto transition-all duration-1000 ease-out ${
+          className={`relative p-8 max-w-lg break-words border rounded-lg border-custom-beige bg-custom-beige mx-auto transition-all duration-1000 ease-out ${
             isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-8'
           }`}
         >
@@ -59,8 +59,8 @@ export default function ConversationScenarioSuggestion({
           <Button className="mt-4" onClick={onClickStartSuggestedScenario}>
             {t('startButton')}
           </Button>
-          <span className="hidden md:block absolute right-[-20px] top-1/2 -translate-y-1/2 w-0 h-0 border-y-[20px] border-y-transparent border-l-[20px] border-l-bw-50"></span>
-          <span className="block md:hidden absolute left-1/2 bottom-[-20px] -translate-x-1/2 w-0 h-0 border-x-[20px] border-x-transparent border-t-[20px] border-t-bw-50"></span>
+          <span className="hidden md:block absolute right-[-20px] top-1/2 -translate-y-1/2 w-0 h-0 border-y-[20px] border-y-transparent border-l-[20px] border-l-custom-beige"></span>
+          <span className="block md:hidden absolute left-1/2 bottom-[-20px] -translate-x-1/2 w-0 h-0 border-x-[20px] border-x-transparent border-t-[20px] border-t-custom-beige"></span>
         </div>
         <Image
           src="/images/mascot.png"
